3440f03b524438550e91e4882309ecab65ad20b2
fj <cuifujian0919@gmail.com>
2024-10-24 05:59:49 -0400

    test constants

--- rust_struct/src/main.rs
+++ rust_struct/src/main.rs
@@ -1,3 +1,22 @@
+
 fn main() {
-    println!("Hello, world!");
+    // test_var2();
+
+   
 }
+
+
+// 覆盖变量
+// fn test_var2(){
+//     let x =5;
+//     println!("this value x is : {x}");
+//     let x = 5+1;
+//     println!("this value x is : {x}");
+//     {
+//         let x = x*2;
+//         println!("this value x is : {x}");
+//     }
+
+//     println!("this value x is : {x}");
+// }
+
---
+++ test_constant/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    println!("Hello, world!");
+
+    const ASDASDASD: i32 = 23;
+
+    println!("ASDASDASD={}",ASDASDASD);
+
+    let x = 5;
+
+    println!("x={}",x);
+
+    let mut y = 12;
+
+    println!("y={}",y);
+
+    y = 45;
+
+    println!("y={}",y);
+
+    let space = "       ";
+
+    println!("space:{}",space);
+
+    let space = space.len();
+
+    println!("space:{}",space);
+
+    let mut test = "asdasds";
+
+
+
+    println!("space:{}",test);
+
+    // test = 123456;
+    // println!("space:{}",test);
+
+}
