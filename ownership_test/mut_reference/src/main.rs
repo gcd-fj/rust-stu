d8804bbaa6b82d48a60313238c2baf9f12f9b780
cuifujian <cuifj@tdhx.com>
2024-12-09 17:45:27 +0800

    MOD

--- ownership_test/mut_reference/src/main.rs
+++ ownership_test/mut_reference/src/main.rs
@@ -3,6 +3,11 @@ fn main() {
 
     let mut s1 = String::from("hello");
 
+    {
+        let r1 = &mut s1;
+        change(r1);
+        println!(" s1 {}",s1)
+    }
     change(&mut s1);
 
     println!("s1 {}",s1)
