52df53ba272c2c268096485235bba8435de854cf
gcd-fj <gcd-fj@users.noreply.github.com>
2026-04-27 17:50:55 +0800

    ownership: study 05 borrow and slice

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,7 +3,10 @@ fn main() {
     // test();
     // test_function();
     // test3();
-    test_tuple();
+    // test_tuple();
+    // test4();
+    // test7();
+    slice();
 }
 
 
@@ -81,9 +84,98 @@ fn test_tuple() {
     println!("s_length: {}, s_length2: {}", s_length, s_length2);
 }
 
+// 引用借用
 #[allow(dead_code)]
-fn calculate_length(s: String) -> (String,usize) {
+fn calculate_length(s: String) -> (String,usize) { // 引用
     let length = s.len();
     (s, length)
 
+}
+
+#[allow(dead_code)]
+fn test4() {
+    let s = String::from("hello");
+    let length = calculate_length2(&s);
+    println!("the length of '{}' is {}.", s, length);
+}
+
+// 借用
+#[allow(dead_code)]
+fn calculate_length2(s: &String) -> usize {
+    s.len()
+}
+
+#[allow(dead_code)]
+fn test5() {
+    // let s = String::from("hello");
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("s: {}", s);
+}
+
+#[allow(dead_code)]
+fn change(some_string: &mut String) {
+    some_string.push_str(", world!");
+}
+
+#[allow(dead_code)]
+fn test6() {
+    let mut s = String::from("hello");
+    let s1 = &mut s;
+    // let s2 = &mut s; // 不支持多个可变引用
+    println!("s1: {}", s1);
+
+    let s3 = &s;
+    let s4 = &s;// 可支持多个不可变引用
+
+    println!("s3: {}, s4: {}", s3, s4);
+    // println!("s1: {}", s1); 不能这样干：s3,s4 已经借走变量，s1 生命周期结束
+
+    // println!("s1: {}, s2: {}", s1, s2);
+}
+
+#[allow(dead_code)]
+fn test7() {
+    let s = dangle();
+    println!("s: {}", s);
+}
+
+// #[allow(dead_code)]
+// fn dangle() -> &String { 此引用会指向一个无效引用，因为生命周期已结束
+//
+//     let s = String::from("hello");
+//     &s
+//
+// }
+
+#[allow(dead_code)]
+fn dangle() -> String { // 所有权被转移
+    let s = String::from("hello");
+    s
+}
+
+#[allow(dead_code)]
+fn slice() {
+    // 另一个没有所有权的数据类型是 slice。slice 允许你引用集合中一段连续的元素序列，而不用引用整个集合。
+    // 字符串 slice（string slice）是 String 中一部分值的引用
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    println!("hello: {}", hello);
+
+    let world = &s[6..11];
+    println!("world: {}", world);
+
+
+    let s = String::from("hello world");
+    let slice = &s[0..2];
+    println!("slice: {}", slice);
+    let slice = &s[..2];
+    println!("slice: {}", slice);
+
+    let len = s.len();
+    let slice = &s[3..len];
+    println!("slice: {}", slice);
+    let slice = &s[3..];
+    println!("slice: {}", slice);
 }
