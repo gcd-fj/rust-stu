6bd8ec08a1ef55854a558766dc29a2e76b3d8046
gcd-fj <gcd-fj@users.noreply.github.com>
2026-05-08 14:56:34 +0800

    study 09: test

---
+++ adder/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
